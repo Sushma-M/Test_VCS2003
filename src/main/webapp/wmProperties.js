c77c4e010809b853a0a10e47b44a83a7fb7b78d7
root <root@3e3164ba0d47>
2016-09-07 14:03:02 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "defaultLanguage" : "en",
+  "displayName" : "Test_VCS23",
+  "homePage" : "Main",
+  "name" : "Test_VCS23",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
